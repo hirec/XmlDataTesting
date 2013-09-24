50dd7ad923d1760d2c0c7e23478397a926acf7c8
Chad Hire <chad_hire@hotmail.com>
2013-09-24 17:05:11 -0400

    Initial upload

---
+++ XmlDataTesting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XmlDataTesting.Utilities;
+
+namespace XmlDataTesting
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      LinqQuery linqQuery = new LinqQuery();
+      var queryResults = linqQuery.QueryData(Directory.GetCurrentDirectory() + "\\SampleData\\netflixStreamingCatalogSampleData.xml", "catalog_title", "title", "short");
+      Console.WriteLine(queryResults);
+      Console.ReadKey();
+    }
+  }
+}
---
+++ XmlDataTesting/Utilities/LinqQuery.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XmlDataTesting.Utilities
+{
+  public class LinqQuery
+  {
+    static IEnumerable<XElement> StreamRootChildDoc(string uri, string rootElement)
+    {
+      using (XmlReader reader = XmlReader.Create(uri))
+      {        
+        reader.MoveToContent();
+        // Parse the file and display each of the nodes.
+        while (reader.Read())
+        {
+          switch (reader.NodeType)
+          {
+            case XmlNodeType.Element:
+              if (reader.Name == rootElement)
+              {
+                XElement el = XElement.ReadFrom(reader) as XElement;
+                if (el != null)
+                  yield return el;
+              }
+              break;
+            case XmlNodeType.CDATA:              
+              break;
+          }
+        }
+      }
+    }
+
+    public object QueryData(string filePath, string rootElement, string queryElement, string queryAttribute)
+    {
+      IEnumerable<string> queriedData =
+              from el in StreamRootChildDoc(filePath, rootElement)
+              where (string)el.Attribute(queryElement) == queryElement
+              select (string)el.Element(queryAttribute);
+      return queriedData;
+    }
+
+  }
+}
